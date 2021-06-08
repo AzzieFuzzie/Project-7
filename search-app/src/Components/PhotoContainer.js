bc42ac7f844abf82826998b3edf96c81142dfef0
AzzieFuzzie <azziefuzzie01@gmail.com>
2021-06-08 09:35:03 +0200

    Update PhotoContainer.js

--- search-app/src/Components/PhotoContainer.js
+++ search-app/src/Components/PhotoContainer.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import Photo from './Photo';
+import Notfound from './Notfound';
 
 const PhotoContainer = (props) => {
   const results = props.data;
-
-  const pictures = results.map((pic) => (
-    <Photo
-      url={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_m.jpg`}
-      key={pic.id}
-      title={pic.title}
-    />
-  ));
+  let pictures;
+  if (results.length > 0) {
+    pictures = results.map((pic) => (
+      <Photo
+        url={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_m.jpg`}
+        key={pic.id}
+        title={pic.title}
+      />
+    ));
+  } else {
+    pictures = <Notfound />;
+  }
 
   return (
     <div className='photo-container'>
